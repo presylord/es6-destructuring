394476416c6266e1e64e2fbc49ce26caae94faa2
Presy Lord Kidlat <91861180+presylord@users.noreply.github.com>
2021-12-04 12:47:31 +0800

    destructuring-practice

--- src/data.js
+++ src/data.js
@@ -1,4 +1,6 @@
 const animals = [
   { name: "cat", sound: "meow" },
-  { name: "dog", sound: "woof" }
+  { name: "dog", sound: "woof", feedingRequirement: { food: 2, water: 3 } }
 ];
+
+export default animals;
--- src/index.js
+++ src/index.js
@@ -1,23 +1,37 @@
 // CHALLENGE: uncomment the code below and see the car stats rendered
-// import React from "react";
-// import ReactDOM from "react-dom";
+import React from "react";
+import ReactDOM from "react-dom";
+import Cars from "./practice";
 
-// ReactDOM.render(
-//   <table>
-//     <tr>
-//       <th>Brand</th>
-//       <th>Top Speed</th>
-//     </tr>
-//     <tr>
-//       <td>{tesla.model}</td>
-//       <td>{teslaTopSpeed}</td>
-//       <td>{teslaTopColour}</td>
-//     </tr>
-//     <tr>
-//       <td>{honda.model}</td>
-//       <td>{hondaTopSpeed}</td>
-//       <td>{hondaTopColour}</td>
-//     </tr>
-//   </table>,
-//   document.getElementById("root")
-// );
+const [honda, tesla] = Cars;
+
+const {
+  coloursByPopularity: [teslaTopColour],
+  speedStats: { topSpeed: teslaTopSpeed }
+} = tesla;
+// const [teslaTopColour, other] = tesla.coloursByPopularity;
+
+const {
+  coloursByPopularity: [hondaTopColour],
+  speedStats: { topSpeed: hondaTopSpeed }
+} = honda;
+
+ReactDOM.render(
+  <table>
+    <tr>
+      <th>Brand</th>
+      <th>Top Speed</th>
+    </tr>
+    <tr>
+      <td>{tesla.model}</td>
+      <td>{teslaTopSpeed}</td>
+      <td>{teslaTopColour}</td>
+    </tr>
+    <tr>
+      <td>{honda.model}</td>
+      <td>{hondaTopSpeed}</td>
+      <td>{hondaTopColour}</td>
+    </tr>
+  </table>,
+  document.getElementById("root")
+);
